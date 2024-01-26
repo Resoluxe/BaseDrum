f7387b62542f495a31e66097a1999098ef346388
Minsoo Kang <davidk1118@gmail.com>
2024-01-26 17:05:48 +0900

    Completely redone the base code by using eventListener!

--- index.js
+++ index.js
@@ -1,49 +1,87 @@
-let i = 0 // Number of seq-rows (Global)
+let totalRow= 1 // Number of seq-rows (Global)
+function toggleColor(current, active, inactive) {
+    if (current.style.backgroundColor === active) {
+        current.style.backgroundColor = inactive;
+    } else current.style.backgroundColor = active;
+}
 
-// The Navigation Bar
+function initializeRow() {
 
-document.getElementById('nuke-all').onclick = nukeAll;
-document.getElementById('add-inst').onclick = addRow;
-document.getElementById('del-inst').onclick = delRow;
-document.getElementById('play-all').onclick = togglePattern;
+    // Make all buttons inactive before doing anything
+    for (let step = 1; step <= 16; step++) {
+        document.querySelector("#seq-row-" + totalRow.toString() + " .steps .step" + step.toString()).style.backgroundColor = "darkgray";
+    }
+    document.querySelector("#seq-row-" + totalRow.toString() + " .mute").style.backgroundColor = "gray";
+    document.querySelector("#seq-row-" + totalRow.toString() + " .solo").style.backgroundColor = "gray";
 
-document.getElementById('conditional').onclick = changeConditionalIcon;
 
-// The Sequencer
-
-// Actual Functions //
-
-function nukeAll() {
-    let nuke_confirm = confirm("This will erase ALL patterns and instruments. Are you really sure?");
-    if (nuke_confirm === true) {
-        for (i; i >= 1; i--){
-            document.querySelector('#grid-container').removeChild(document.querySelectorAll("div[id^='seq-row']")[i]);
+    // Enable Row Clear
+    document.querySelector("#seq-row-" + totalRow.toString() + " .row-clear").addEventListener('click', function rowClear() {
+        for (let step = 1; step <= 16; step++) {
+            document.querySelector("#seq-row-" + this.textContent + " .steps .step" + step.toString()).style.backgroundColor = "darkgray";
         }
-        i = 0;
+    });
+
+    // Enable Mute Function
+    document.querySelector("#seq-row-" + totalRow.toString() + " .mute").addEventListener('click', function muteRow() {
+        toggleColor(this, "red", "gray");
+    });
+
+    // Enable Solo Function
+    document.querySelector("#seq-row-" + totalRow.toString() + " .solo").addEventListener('click', function soloRow() {
+        toggleColor(this, "orange", "gray");
+    });
+
+    // Toggle Step
+    for (let step = 1; step <= 16; step++) {
+        document.querySelector("#seq-row-" + totalRow.toString() + " .steps .step" + step.toString()).addEventListener('click', function toggleStep() {
+            toggleColor(this, "lawngreen", "darkgray");
+        });
     }
 }
 
-function addRow() {
-    let new_row = document.querySelector("#seq-row").cloneNode(true);
-    new_row.setAttribute("id", "seq-row-" + (i + 1).toString());
-    new_row.querySelector(".row-index").textContent = (i + 1).toString();
+initializeRow();
+
+/* Navigation Bar */
+
+// Erase all pattern (Nuke)
+document.querySelector("#nuke-all").addEventListener('click', function nukeAll() {
+    let nukeConfirm = confirm("This will ERASE ALL patterns. Are you really sure?");
+    if (nukeConfirm === true) {
+        document.querySelectorAll(".steps *").forEach((element) => element.style.backgroundColor = "darkgray");
+    }
+});
+
+
+// Add an instrument
+document.querySelector("#add-inst").addEventListener('click', function addRow() {
+
+    // Add a new row
+    let new_row = document.querySelector("#seq-row-1").cloneNode(true);
+    totalRow++;
+    new_row.setAttribute("id", "seq-row-" + totalRow.toString());
+    new_row.querySelector(".row-clear").textContent = totalRow.toString();
     let targetContainer= document.querySelector('#grid-container');
     targetContainer.appendChild(document.importNode(new_row, true));
-    i++;
-}
-function delRow() {
-    if (i > 0) {
-        document.getElementById("seq-row-" + i.toString()).remove();
-        i--;
+    initializeRow();
+
+});
+
+// Remove the lowest instrument
+document.querySelector("#del-inst").addEventListener('click', function delInst() {
+    if (totalRow > 1) {
+        document.querySelector("#seq-row-" + totalRow.toString()).remove();
+        totalRow--;
     }
-}
+});
 
-function togglePattern() {
-    if (document.getElementById('play-all').style.backgroundColor === "lawngreen") {
-        document.getElementById('play-all').style.backgroundColor = "gray";
-    } else document.getElementById('play-all').style.backgroundColor = "lawngreen";
-}
+// Play through pattern
+document.querySelector("#play-all").addEventListener('click', function togglePattern() {
+    toggleColor(this, "lawngreen", "gray");
+});
 
-function changeConditionalIcon() {
-    document.getElementById('conditional').textContent = ['⚀COND', '⚁COND', '⚂COND', '⚃COND', '⚄COND', '⚅COND'][Math.floor(6 * Math.random())];
-}
+// Activate Conditional View + Change Icon
+document.querySelector("#conditional").addEventListener('click', function toggleConditional() {
+    this.textContent = ['⚀COND', '⚁COND', '⚂COND', '⚃COND', '⚄COND', '⚅COND'][Math.floor(6 * Math.random())];
+    toggleColor(this, "lawngreen", "gray");
+});
