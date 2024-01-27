7a14deec83e9f966324dfadf963590666341c8d3
Minsoo Kang <davidk1118@gmail.com>
2024-01-27 09:12:12 +0900

    Added (Rudimentary) file input

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-let totalRow= 1 // Number of seq-rows (Global)
+let totalRow= 1; // Number of seq-rows (Global)
+let bpm = 120; // BPM (Global)
+let pos = 1; // Current Position (within 16)
+
 function toggleColor(current, active, inactive) {
     if (current.style.backgroundColor === active) {
         current.style.backgroundColor = inactive;
-    } else current.style.backgroundColor = active;
+    } else if (current.style.backgroundColor === inactive) {
+        current.style.backgroundColor = active;
+    }
+}
+
+function toggleColumn(position) {
+    document.querySelectorAll(".step" + position.toString()).forEach((element) => toggleColor(element, "orange", "lawngreen"));
+}
+
+function patternPlay() {
+    for (pos; pos <= 16; pos++) {
+        if (pos === 1) {
+            toggleColumn(1);
+            toggleColumn(16);
+        } else {
+            toggleColumn(pos);
+            toggleColumn(pos - 1)
+            if (pos === 16) {
+                pos = 1
+            }
+        }
+    }
 }
 
 function initializeRow() {
@@ -21,6 +45,8 @@ function initializeRow() {
             document.querySelector("#seq-row-" + this.textContent + " .steps .step" + step.toString()).style.backgroundColor = "darkgray";
         }
     });
+
+    //Display the Uploaded Sample when Uploading
 
     // Enable Mute Function
     document.querySelector("#seq-row-" + totalRow.toString() + " .mute").addEventListener('click', function muteRow() {
@@ -48,7 +74,7 @@ initializeRow();
 document.querySelector("#nuke-all").addEventListener('click', function nukeAll() {
     let nukeConfirm = confirm("This will ERASE ALL patterns. Are you really sure?");
     if (nukeConfirm === true) {
-        document.querySelectorAll(".steps *").forEach((element) => element.style.backgroundColor = "darkgray");
+        document.querySelectorAll(".steps button").forEach((element) => element.style.backgroundColor = "darkgray");
     }
 });
 
@@ -61,6 +87,9 @@ document.querySelector("#add-inst").addEventListener('click', function addRow() 
     totalRow++;
     new_row.setAttribute("id", "seq-row-" + totalRow.toString());
     new_row.querySelector(".row-clear").textContent = totalRow.toString();
+    new_row.querySelector("label").setAttribute("for", "inst-" + totalRow.toString());
+    new_row.querySelector("label").setAttribute("id", "new-inst-" + totalRow.toString());
+    new_row.querySelector("input").setAttribute("id", "inst-" + totalRow.toString());
     let targetContainer= document.querySelector('#grid-container');
     targetContainer.appendChild(document.importNode(new_row, true));
     initializeRow();
@@ -75,10 +104,17 @@ document.querySelector("#del-inst").addEventListener('click', function delInst()
     }
 });
 
-// Play through pattern
-document.querySelector("#play-all").addEventListener('click', function togglePattern() {
-    toggleColor(this, "lawngreen", "gray");
+
+document.querySelector("#bpm-label").addEventListener('input', function setBPM() {
+    if (document.querySelector("#bpm").value > 0 || document.querySelector("#bpm").value === "") {
+        bpm = 60000 / document.querySelector("#bpm").value;
+    } else {
+        alert("BPM must be higher than 0!");
+        document.querySelector("#bpm").value = ""
+    }
 });
+
+// Play through pattern
 
 // Activate Conditional View + Change Icon
 document.querySelector("#conditional").addEventListener('click', function toggleConditional() {
